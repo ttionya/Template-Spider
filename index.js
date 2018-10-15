1540dd2f17ce9ba2d1f8d8a157e5bce16448a35a
ttionya <git@ttionya.com>
2018-10-16 00:21:23 +0800

    升级包，兼容 Babel 7 等

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-import "babel-polyfill";
 // import superAgent from 'superagent';
 // import cheerio from 'cheerio';
 // import { mapLimit } from 'async';
 import fetch from './libs/fetch';
 // import logger from './libs/logger';
-
 
 
 fetch([1]);
